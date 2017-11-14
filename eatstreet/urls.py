17c3d2e9f93cd95d837f19f62399a07d71b9b8d3
Subhash Hardaha <subhashhardaha@gmail.com>
2017-11-15 00:26:49 +0530

    Model Added

--- eatstreet/urls.py
+++ eatstreet/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from restaurants.views import home, about, contact
+from restaurants.views import home, about, contact,restaurant_listview
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',home),
     url(r'^about/', about),
     url(r'^contact/', contact),
+    url(r'^restaurant/', restaurant_listview),
+
 
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -3,6 +3,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Restaurant
+
+def restaurant_listview(request):
+    template_name='restaurants/restaurants_list.html'
+    queryset=Restaurant.objects.all()
+    context={
+        "object_list":queryset
+    }
+    return render(request,template_name,context)
+
+
+
+
+
+
+
+
+
+
+
 
 def home(request):
     context={
